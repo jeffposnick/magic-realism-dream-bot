5afa9a9edd27be05df882d5e59963a04b85c1d1b
Jeff Posnick <jeffy@google.com>
2022-09-02 09:17:08 -0400

    Skip if done

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import log from 'loglevel';
 import prefix from 'loglevel-plugin-prefix';
 
-import {getLatestTweet, sendTweet} from './twitter';
+import {getTweetIfNew, sendTweet} from './twitter';
 import {generateImage} from './stable-diffusion';
 
 prefix.reg(log);
@@ -9,9 +9,14 @@ prefix.apply(log);
 log.enableAll();
 
 log.info(`Starting up...`);
-const latestTweet = await getLatestTweet();
-log.info(`Latest tweet: ${latestTweet.full_text}`);
-const {imageBuffer, mimeType} = await generateImage(latestTweet.full_text!);
-log.info(`Generated image.`);
-await sendTweet(imageBuffer, mimeType, latestTweet.id_str);
-log.info(`Posted tweet.`);
+const latestTweet = await getTweetIfNew();
+if (latestTweet) {
+	log.info(`Latest tweet: ${latestTweet.full_text}`);
+	const {imageBuffer, mimeType} = await generateImage(latestTweet.full_text!);
+	log.info(`Generated image.`);
+	await sendTweet(imageBuffer, mimeType, latestTweet.id_str);
+	log.info(`Posted tweet.`);
+} else {
+	log.info(`Latest tweet has already been processed.`);
+}
+log.info(`...all done.`);
--- src/twitter.ts
+++ src/twitter.ts
@@ -13,11 +13,28 @@ const client = new TwitterApi({
 	appSecret: process.env.TWITTER_APP_SECRET!,
 }).readWrite;
 
-export async function getLatestTweet() {
-	const [latestTweet] = await client.v1.userTimeline(
+function tweetIdToURL(id: string) {
+	return TWEET_URL_PREFIX + id;
+}
+
+export async function getTweetIfNew() {
+	const [latestMagicalRealismTweet] = await client.v1.userTimeline(
 		MAGICAL_REALISM_TWITTER_ID,
 	);
-	return latestTweet;
+	const magicalRealismTweetURL = tweetIdToURL(latestMagicalRealismTweet.id_str);
+
+	const bot = await client.currentUser();
+	const [latestBotTweet] = await client.v1.userTimeline(bot.id_str);
+	const urls = latestBotTweet.entities?.urls;
+
+	if (urls?.length) {
+		const latestBotTweetReferenceURL = urls[0].expanded_url;
+		if (latestBotTweetReferenceURL === magicalRealismTweetURL) {
+			return null;
+		}
+	}
+
+	return latestMagicalRealismTweet;
 }
 
 export async function sendTweet(
@@ -26,7 +43,7 @@ export async function sendTweet(
 	tweetId: string,
 ) {
 	const mediaId = await client.v1.uploadMedia(imageBuffer, {mimeType});
-	await client.v1.tweet(TWEET_URL_PREFIX + tweetId, {
+	await client.v1.tweet(tweetIdToURL(tweetId), {
 		media_ids: mediaId,
 	});
 }
