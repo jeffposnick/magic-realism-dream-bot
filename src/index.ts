0bf7c5b7eacf5af364a6c30ee208e07062d1fe1b
Jeff Posnick <jeffy@google.com>
2022-09-02 07:27:56 -0400

    Added logging with timestamps

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
+import log from 'loglevel';
+import prefix from 'loglevel-plugin-prefix';
+
 import {getLatestTweet, sendTweet} from './twitter';
 import {generateImage} from './stable-diffusion';
 
+prefix.reg(log);
+prefix.apply(log);
+log.enableAll();
+
+log.info(`Starting up...`);
 const latestTweet = await getLatestTweet();
+log.info(`Latest tweet: ${latestTweet.full_text}`);
 const {imageBuffer, mimeType} = await generateImage(latestTweet.full_text!);
+log.info(`Generated image.`);
 await sendTweet(imageBuffer, mimeType, latestTweet.id_str);
+log.info(`Posted tweet.`);
