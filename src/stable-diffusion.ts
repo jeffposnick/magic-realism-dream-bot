6b238042904f30c9b007bfc9a011ba8db2ac120b
Jeff Posnick <jeffy@google.com>
2022-09-03 08:28:35 -0400

    Better errors

--- src/stable-diffusion.ts
+++ src/stable-diffusion.ts
@@ -98,7 +98,7 @@ export function generateImage(partialPrompt: string) {
 				}
 
 				if (artifact.text) {
-					return reject(artifact.text);
+					return reject(`${artifact.finishReason}: ${artifact.text}`);
 				}
 			}
 		});
